fdd81a74cc7c685b9c3a62439cf9b9a6f4b768aa
Thinh Phan <thinh.phan.18@cnu.edu>
2021-01-04 13:56:37 -0500

    update

--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import Question from "./Question"
+import {Paper, withStyles} from "@material-ui/core"
 
-function Box() {
+const styles ={
+    box: {
+        borderRadius: '10px',
+    },
+
+    question: {
+        width: '50%',
+        margin: '0 auto'
+    }
+};
+
+function Box(props) {
+    const { classes, children, className, ...other } = props;
     return (
-        <div className="box">
-            <Question/>
-        </div>
-    )
+        <div className={classes.box}>
+            <Paper className={classes.question}>          
+                <h4>Question</h4>
+                <p> 1 + 1 = ?</p>
+            </Paper>
+            
+        </div>  
+    );
 }
 
-export default Box
+export default withStyles(styles)(Box);
--- src/Header.js
+++ src/Header.js
@@ -13,9 +13,9 @@ function Header() {
             
             <div className="header_right">
                 <div className="header_info">
-                    <Avatar src="https://i.pinimg.com/originals/da/be/ea/dabeea3f3cf5e435bd7700eaf55c06a3.jpg" alt=""/>
+                    <Avatar src="https://scontent.forf1-3.fna.fbcdn.net/v/t1.0-9/116607019_613009926265569_2852008619302352282_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=dEFfIY9TvcwAX_n5TiB&_nc_ht=scontent.forf1-3.fna&oh=21a244d019f9df18f6ee100be416402c&oe=60171EFE" alt=""/>
 
-                    <h4>User name (add later) </h4>
+                    <h4>Minh Hoang </h4>
                 </div>
                 <div className="header_option">
                     <ExitToAppIcon fontSize="large"></ExitToAppIcon>
